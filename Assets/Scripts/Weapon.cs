ff233b261d827e47a88170af0be8d483a865a6ac
abaddon-dev <pawel.dwornicki86@gmail.com>
2022-01-15 20:14:15 +0100

    Added muzzle flash and hit effect.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,15 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
+    [SerializeField] float damage = 50f;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect;
+
+    EnemyHealth enemyHealth;
+
+    void Start() {
+        enemyHealth = GetComponent<EnemyHealth>();
+    }
 
     void Update()
     {
@@ -17,9 +27,34 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
-        RaycastHit hit; 
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
-        
-        Debug.Log("You hit " + hit.transform.name);
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            CreateHitImpact(hit);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {        
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));   
+        Destroy(impact, 0.2f);
     }
 }
